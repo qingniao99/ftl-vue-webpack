047c5e634560b92f8cfbae7041471c2f02c15790
qingniao99 <huangjianqiang126@163.com>
2017-05-02 14:05:34 +0800

    add normalize and lib.flexible

--- src/module/index/App.vue
+++ src/module/index/App.vue
@@ -1,20 +1,21 @@
 <template>
-	<vindex></vindex>
+  <vindex></vindex>
 </template>
 
 <script>
-import vindex from '../../components/index/Index'
+  import "normalize.css/normalize.css";
+  import vindex from '../../components/index/Index'
 
-export default {
-    name:"app",
-	components: {
+  export default {
+    name: "app",
+    components: {
       // 可以以key-value的形式注册组件, 此时挂载点的名字就是key
       // 否则挂载点和组件名字一致, 即vhead
       vindex
     }
-}
+  }
 </script>
 
 <style>
-
+h1{font-size: 100px;}
 </style>
